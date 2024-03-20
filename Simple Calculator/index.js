55aacdde58d2da00f2e8d917af48ead64e4c8c3e
Rao Hamza Tariq <147372279+RaoHamzaTariq@users.noreply.github.com>
2024-03-20 13:22:50 +0500

    Add files via upload

---
+++ Simple Calculator/index.js
@@ -0,0 +1,31 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Enter the operator",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+    }
+]);
+function SimpleCalculator(answer) {
+    let { firstNumber, secondNumber, operator } = answer;
+    if (operator == "Addition") {
+        console.log(`The answer is ${firstNumber + secondNumber}`);
+    }
+    else if (operator == "Subtraction") {
+        console.log(`The answer is ${firstNumber - secondNumber}`);
+    }
+    else if (operator == "Multiplication") {
+        console.log(`The answer is ${firstNumber * secondNumber}`);
+    }
+    else if (operator == "Division") {
+        console.log(`The answer is ${firstNumber / secondNumber}`);
+    }
+    else {
+        console.log("Please select a valid operator");
+    }
+}
+SimpleCalculator(answer);
--- Simple Calculator/index.ts
+++ Simple Calculator/index.ts
@@ -1,1 +1,38 @@
+#! /usr/bin/env node
 
+import inquirer from "inquirer";
+
+type DataType = {
+    operator: string;
+    firstNumber: number;
+    secondNumber: number;
+}
+
+const answer = await inquirer.prompt([
+  { message: "Enter first number", type: "number", name: "firstNumber" },
+  { message: "Enter second number", type: "number", name: "secondNumber" },
+  {
+    message: "Enter the operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division"]
+  }
+]);
+
+function SimpleCalculator(answer: DataType) {
+    let { firstNumber, secondNumber, operator } = answer;
+
+    if (operator == "Addition") {
+        console.log(`The answer is ${firstNumber + secondNumber}`);
+    } else if (operator == "Subtraction") {
+        console.log(`The answer is ${firstNumber - secondNumber}`);
+    } else if (operator == "Multiplication") {
+        console.log(`The answer is ${firstNumber * secondNumber}`);
+    } else if (operator == "Division") {
+        console.log(`The answer is ${firstNumber / secondNumber}`);
+    } else {
+        console.log("Please select a valid operator");
+    }
+}
+
+SimpleCalculator(answer);
